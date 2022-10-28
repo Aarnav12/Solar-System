81a39183a3d0bee7ee9601992f5ef743f50875d7
Aarnav12 <74446681+Aarnav12@users.noreply.github.com>
2022-10-28 15:47:16 +0530

    Add files via upload

--- Solar-System.py
+++ Solar-System.py
@@ -2,14 +2,77 @@ import cv2
 
 img = cv2.imread("C:/Users/sapsl/Downloads/Whitehat jr Python/C104/Image/solar-system.jpg")
 
-text_to_show = "Sun"
-
 
 cv2.putText(img,  
-           text_to_show,
+           "Sun",
            (85, 100),  
            fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
            fontScale=1,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Mercury",
+           (125, 190),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.5,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Venus",
+           (195, 175),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Earth",
+           (285, 170),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Mars",
+           (380, 180),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Jupiter",
+           (475, 100),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Saturn",
+           (670, 120),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+
+cv2.putText(img,  
+           "Uranus",
+           (965, 140),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
+           color=(0,0,255)
+           )
+
+cv2.putText(img,  
+           "Neptune",
+           (1110, 145),  
+           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
+           fontScale=0.6,  
            color=(0,0,255)
            )
 
